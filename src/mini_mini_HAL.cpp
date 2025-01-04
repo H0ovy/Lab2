57f5417f03405ed78be318b4e3825f7c8f4b1ae2
H0ovy <m.dorogof@gmail.com>
2025-01-04 21:48:08 +0700

    Update

--- src/mini_mini_HAL.cpp
+++ src/mini_mini_HAL.cpp
@@ -1,5 +1,10 @@
 #include "mini_mini_HAL.h"
 
+Mini_mini_HAL::Mini_mini_HAL()
+{
+
+}
+
 void Mini_mini_HAL::PinMode(Uint8_t pin, Uint8_t mode)
 {
 	Uint8_t bit = digitalPinToBitMask(pin);
@@ -36,20 +41,162 @@ void Mini_mini_HAL::PinMode(Uint8_t pin, Uint8_t mode)
 	}
 }
 
+#define _LPM_word_enhanced__(addr)         \
+(__extension__({                            \
+    uint16_t __addr16 = (uint16_t)(addr);   \
+    uint16_t __result;                      \
+    __asm__ __volatile__                    \
+    (                                       \
+        "lpm %A0, Z+"   "\n\t"              \
+        "lpm %B0, Z"    "\n\t"              \
+        : "=r" (__result), "=z" (__addr16)  \
+        : "1" (__addr16)                    \
+    );                                      \
+    __result;                               \
+}))
+const Uint16_t PROGMEM Port_to_output_PGM[] = {
+	NOT_A_PORT,
+	NOT_A_PORT,
+	(uint16_t) &PORTB,
+	(uint16_t) &PORTC,
+	(uint16_t) &PORTD,
+};
+
+volatile Uint8_t * Mini_mini_HAL::PortOutputRegister(Uint8_t P)
+{
+	return (volatile Uint8_t *)( _LPM_word_enhanced__( Port_to_output_PGM + (P)));
+}
+
+const Uint8_t PROGMEM Digital_pin_to_timer_PGM[] = {
+	NOT_ON_TIMER, /* 0 - port D */
+	NOT_ON_TIMER,
+	NOT_ON_TIMER,
+	// on the ATmega168, digital pin 3 has hardware pwm
+#if defined(__AVR_ATmega8__)
+	NOT_ON_TIMER,
+#else
+	TIMER2B,
+#endif
+	NOT_ON_TIMER,
+	// on the ATmega168, digital pins 5 and 6 have hardware pwm
+#if defined(__AVR_ATmega8__)
+	NOT_ON_TIMER,
+	NOT_ON_TIMER,
+#else
+	TIMER0B,
+	TIMER0A,
+#endif
+	NOT_ON_TIMER,
+	NOT_ON_TIMER, /* 8 - port B */
+	TIMER1A,
+	TIMER1B,
+#if defined(__AVR_ATmega8__)
+	TIMER2,
+#else
+	TIMER2A,
+#endif
+	NOT_ON_TIMER,
+	NOT_ON_TIMER,
+	NOT_ON_TIMER,
+	NOT_ON_TIMER, /* 14 - port C */
+	NOT_ON_TIMER,
+	NOT_ON_TIMER,
+	NOT_ON_TIMER,
+	NOT_ON_TIMER,
+};
+
+
+
+#define _LPM_enhanced__(addr)  \
+(__extension__({                \
+    uint16_t __addr16 = (uint16_t)(addr); \
+    uint8_t __result;           \
+    __asm__ __volatile__        \
+    (                           \
+        "lpm %0, Z" "\n\t"      \
+        : "=r" (__result)       \
+        : "z" (__addr16)        \
+    );                          \
+    __result;                   \
+}))
+
+Uint8_t Mini_mini_HAL::DigitalPinToTimer(Uint8_t P)
+{
+	return _LPM_enhanced__(Digital_pin_to_timer_PGM + (P));
+}
+
+const Uint8_t PROGMEM Digital_pin_to_bit_mask_PGM[] = {
+	_BV(0), /* 0, port D */
+	_BV(1),
+	_BV(2),
+	_BV(3),
+	_BV(4),
+	_BV(5),
+	_BV(6),
+	_BV(7),
+	_BV(0), /* 8, port B */
+	_BV(1),
+	_BV(2),
+	_BV(3),
+	_BV(4),
+	_BV(5),
+	_BV(0), /* 14, port C */
+	_BV(1),
+	_BV(2),
+	_BV(3),
+	_BV(4),
+	_BV(5),
+};
+
+Uint8_t Mini_mini_HAL::DigitalPinToBitMask(Uint8_t P)
+{
+	return _LPM_enhanced__(Digital_pin_to_bit_mask_PGM + (P));
+}
+
+const Uint8_t PROGMEM Digital_pin_to_port_PGM[] = {
+        4, /* 0 */
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        2, /* 8 */
+        2,
+        2,
+        2,
+        2,
+        2,
+        3, /* 14 */
+        3,
+        3,
+        3,
+        3,
+        3,
+    };
+
+Uint8_t Mini_mini_HAL::DigitalPinToPort(Uint8_t P)
+{
+	return _LPM_enhanced__(Digital_pin_to_port_PGM + (P));
+}
+
 void Mini_mini_HAL::DigitalWrite(Uint8_t pin, Uint8_t val)
 {
-    Uint8_t timer = digitalPinToTimer(pin);
-	Uint8_t bit = digitalPinToBitMask(pin);
-	Uint8_t port = digitalPinToPort(pin);
+    Uint8_t timer = DigitalPinToTimer(pin);
+	Uint8_t bit = DigitalPinToBitMask(pin);
+	Uint8_t port = DigitalPinToPort(pin);
 	volatile Uint8_t *out;
 
-	if (port == NOT_A_PIN) return;
+	if (port == 0) 
+		return;
 
 	// If the pin that support PWM output, we need to turn it off
 	// before doing a digital write.
-	if (timer != NOT_ON_TIMER) TurnOffPWM(timer);
+	if (timer != 0) 
+		TurnOffPWM(timer);
 
-	out = portOutputRegister(port);
+	out = PortOutputRegister(port);
 
 	Uint8_t oldSREG = SREG;
 	cli();
@@ -126,18 +273,6 @@ void Mini_mini_HAL::TurnOffPWM(Uint8_t timer)
 	}
 }
 
-// void Mini_mini_HAL::Delay(unsigned long ms)
-// {
-// 	uint32_t start = micros();
-
-// 	while (ms > 0) {
-// 		yield();
-// 		while ( ms > 0 && (micros() - start) >= 1000) {
-// 			ms--;
-// 			start += 1000;
-// 		}
-// 	}
-// }
 
 int Mini_mini_HAL::DigitalRead(Uint8_t pin)
 {
